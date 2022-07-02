3d0551e5e97c202fef249f37e54a24bb99a438bb
윤경 <yg@yungyeong-ui-MacBook-Pro.local>
2022-07-02 20:36:12 +0900

    20220702 H-Index (Arrays.sort)

--- javaProgramming/src/programmers/scoreKit/sort/HIndex.java
+++ javaProgramming/src/programmers/scoreKit/sort/HIndex.java
@@ -2,28 +2,21 @@ package programmers.scoreKit.sort;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.Scanner;
 
 public class HIndex {
     public static int solution(int[] citations) {
-        int answer = 0, sum = 0;
-        ArrayList<Integer> list = new ArrayList<>();
+        int answer = 0;
+        Arrays.sort(citations);
 
-        for (int num:citations) {
-            if (num != 0) sum += num;
-            list.add(num);
+        for (int i=0; i<citations.length; i++) {
+            int h = citations.length - i;
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
         }
-
-        if ( sum == 0 ) return 0;
-        Collections.sort(list);
-
-        for (int i=list.size()-1; i > -1; i--) {
-            if ( i+1 == list.get(i)) return i+1;
-        }
-
         return answer;
     }
 
